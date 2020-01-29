7d86e9ed221505b0144a0fcfc69e3448dac20ba1
Inam Ul Haq <inambe.dev@gmail.com>
2020-01-29 11:37:30 +0500

    corrected db connection

--- core/pool.js
+++ core/pool.js
@@ -3,7 +3,12 @@ var mysql = require("mysql");
 /**
  * Connection to the database.
  *  */
-var pool = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+var pool = mysql.createPool({
+	host: "us-cdbr-iron-east-04.cleardb.net",
+	user: "b985dc3b3b241d", // use your mysql username.
+	password: "0d98a743", // user your mysql password.
+	database: "heroku_fd85a10cd896b1c"
+});
 
 pool.getConnection((err, connection) => {
 	if (err)
